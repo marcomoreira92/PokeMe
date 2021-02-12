1c882b6ac1208bcc5c342032c9c56dbbad1c2e5d
mmalaqui <mmalaqio@everis.com>
2021-02-12 15:06:40 +0000

    added base menu for app

---
+++ PokeMe/PokeMe/BaseUIElements/Menu.swift
@@ -0,0 +1,41 @@
+//
+//  Menu.swift
+//  PokeMe
+//
+//  Created by mmalaqui on 12/02/2021.
+//
+
+import UIKit
+
+class Menu: UIView {
+
+    @IBOutlet var view: UIView!
+    
+    @IBOutlet weak var historyButton: UIButton!
+    @IBOutlet weak var listButton: UIButton!
+    @IBOutlet weak var pokeBallButton: UIButton!
+    
+    let nibName = "Menu"
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    func commonInit() {
+        guard let view = loadViewFromNib() else { return }
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
+    
+    func loadViewFromNib() -> UIView? {
+        let nib = UINib(nibName: nibName, bundle: nil)
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
+
+}
