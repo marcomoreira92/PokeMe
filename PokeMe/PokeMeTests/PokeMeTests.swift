8e9aca5ce3b0882278644b7f2618a2cd7b5a8754
mmalaqui <mmalaqio@everis.com>
2021-02-17 14:12:21 +0000

    added cache test

--- PokeMe/PokeMeTests/PokeMeTests.swift
+++ PokeMe/PokeMeTests/PokeMeTests.swift
@@ -50,6 +50,33 @@ class PokeMeTests: XCTestCase {
         
     }
     
+    func test_cdl_pokemon_by_id_from_cache() throws {
+
+        let expectation = XCTestExpectation(description: "calls CDL layer for specific pokemon")
+
+        pokemonCDL.getPokemonByID(pokemonID: "1", subscriber: ("CDLtest", { ( response: CDLResponse? ) -> Void in
+
+            let pokemonFromCache = CommonDataLayer.shared.returnFromCache(cacheID: "1")
+            XCTAssert(pokemonFromCache != nil)
+            
+            if let model = pokemonFromCache as? CDLPokemonModel {
+                XCTAssert(model.name == "bulbasaur")
+                XCTAssert(model.stats?.count != 0)
+                XCTAssert(model.stats?[0].base_stat != nil)
+                XCTAssert(model.stats?[0].stat != nil)
+                XCTAssert(model.stats?[0].stat?.name != nil)
+                XCTAssert(model.sprites != nil)
+                XCTAssert(model.sprites?.other != nil)
+                XCTAssert(model.sprites?.other?.officialartwork != nil)
+            }else{
+                XCTAssert(false, "model is not compatible with CDL pokemon")
+            }
+        
+            expectation.fulfill()
+        }))
+        wait(for: [expectation], timeout: 1)
+    }
+    
     func test_home_interactor_get_pokemon_by_valid_id() throws {
         let expectation = XCTestExpectation(description: "calls home interactor layer for specific pokemon")
         let homeInteractor = HomeInteractor()
