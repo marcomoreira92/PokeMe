cc469988bc94fbf95e265a8ef54b44cc48e79a66
mmalaqui <mmalaqio@everis.com>
2021-02-17 11:24:53 +0000

    added SDWebImage Pod

---
+++ PokeMe/PokeMe/BaseUIElements/PokeCardCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PokeCardCollectionViewCell.swift
+//  PokeMe
+//
+//  Created by mmalaqui on 15/02/2021.
+//
+
+import UIKit
+
+class PokeCardCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
