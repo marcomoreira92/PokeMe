fedb2cc65f7ce8a4469805b97ead918a3caa65d2
mmalaqui <mmalaqio@everis.com>
2021-02-17 15:07:54 +0000

    added pokemonStatsCollectionviewCell (empty)

---
+++ PokeMe/PokeMe/BaseUIElements/Cells/PokemonStatsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonStatsCollectionViewCell.swift
+//  PokeMe
+//
+//  Created by mmalaqui on 17/02/2021.
+//
+
+import UIKit
+
+class PokemonStatsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
